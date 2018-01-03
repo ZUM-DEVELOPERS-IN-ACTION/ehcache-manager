e4aa089fd918c2a38676c7ce1482199f092c4e57
ChoiJoongHyun <joonghyun0117@gmail.com>
2018-01-03 11:24:41 +0900

    first commit

---
+++ src/main/java/com/zum/ehcache/manager/EhcacheManagerApplication.java
@@ -0,0 +1,12 @@
+package com.zum.ehcache.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhcacheManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EhcacheManagerApplication.class, args);
+	}
+}
